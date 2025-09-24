cb8ac175a707fd06b3ef24e8b968638ac3a315e8
aziscout <abdulaziscout@gmail.com>
2025-09-24 23:22:21 +0800

    update route produk hukum

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/produk-hukum/create', [ProdukHukumController::class, 'create'])->name('produk-hukum.create');
     Route::post('/produk-hukum', [ProdukHukumController::class, 'store'])->name('produk-hukum.store');
     Route::get('/produk-hukum/create', [ProdukHukumController::class, 'create'])->name('produk-hukum.create');
+    Route::get('/produk-hukum/{produkHukum}/edit', [ProdukHukumController::class, 'edit'])->name('produk-hukum.edit');
+    Route::put('/produk-hukum/{produkHukum}', [ProdukHukumController::class, 'update'])->name('produk-hukum.update');
+    Route::delete('/produk-hukum/{produkHukum}', [ProdukHukumController::class, 'destroy'])->name('produk-hukum.destroy');
 });
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
